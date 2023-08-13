c677a838aa3dcada0cddeef2b8a4093a92aaa86a
Saurlax <saurlax@qq.com>
2023-08-13 23:00:06 +0800

    feat: support vivia@0.0.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const template = require('art-template')
 module.exports = config => {
   return ctx => {
     ctx.type = 'html'
+    ctx.content = ctx.content.toString()
     ctx.content = template.render(ctx.template, ctx, config)
   }
 }
