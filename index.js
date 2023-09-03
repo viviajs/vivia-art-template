e188c80ab6adeecc50aba9c9c2b2a5217cab608c
Saurlax <saurlax@qq.com>
2023-09-03 21:23:21 +0800

    fix: make config for art-template work

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = config => {
   return context => {
     context.path = context.path.replace(/\.[^\.]+$/, '.html')
     context.content = context.content.toString()
-    context.content = template(context.template, context)
+    context.content = template.compile({
+      ...config,
+      filename: context.template
+    })(context)
   }
 }
