6d32a7e438a79bba32caa0a392110e645197eb6d
Saurlax <saurlax@qq.com>
2023-09-03 19:59:59 +0800

    feat: support vivia@0.1.0

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = config => {
   return context => {
     context.path = context.path.replace(/\.[^\.]+$/, '.html')
     context.content = context.content.toString()
-    context.content = template.render(context.template, context, config)
+    context.content = template(context.template, context)
   }
 }
