c32c7df7859a218f25aed3c3e9f943ece91e080d
Saurlax <saurlax@qq.com>
2023-10-17 17:57:22 +0800

    feat: add built-in class

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const template = require('art-template')
+const path = require('path')
 
 module.exports = config => {
+  options = {
+    ...template.defaults,
+    debug: config.debug,
+    debug: false,
+    minimize: true,
+    htmlMinifierOptions: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+    },
+    root: path.resolve('template'),
+    imports: {
+      Object,
+      Array,
+      Date,
+      Math
+    },
+    ...config
+  }
+
   return context => {
     context.path = context.path.replace(/\.[^\.]+$/, '.html')
     context.content = context.content.toString()
-    context.content = template.compile({
-      ...config,
-      filename: context.template
-    })(context)
+    context.content = template.compile({ ...options, filename: context.template })(context)
   }
 }
